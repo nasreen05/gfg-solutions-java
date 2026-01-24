5a9e55abfe3c53d8c495d75f0d8e9b3f7761aabb
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-24 14:51:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        
+        int[] count = new int[26];
+        
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
