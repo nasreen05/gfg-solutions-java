00d0fd8489fb0fc203159059067e1e220bccf93f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-17 13:25:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort Method/sort-method.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void sortArray(int arr[]) {
+        
+        Arrays.sort(arr);
+        
+        // No need to print anything
+        // The array gets sorted in-place
+    }
+}
