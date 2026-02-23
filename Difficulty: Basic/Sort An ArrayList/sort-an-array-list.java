f24ec4c74fd23faa5160b77be830896556f59d02
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-23 15:25:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort An ArrayList/sort-an-array-list.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+class Solution {
+    public static void sortArrayList(ArrayList<Integer> list) {
+        
+        // Sort the ArrayList in increasing order
+        Collections.sort(list);
+    }
+}
