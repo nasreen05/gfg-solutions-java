5f6b2f761af14011487f3af14cb18096f32c00a6
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-03-06 12:33:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/String Concatenation/string-concatenation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static String concatenateStrings(ArrayList<String> arr) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String s : arr){
+            sb.append(s);
+        }
+        
+        return sb.toString();
+    }
+}
