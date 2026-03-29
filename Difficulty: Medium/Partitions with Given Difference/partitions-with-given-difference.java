476c4c3e9971f1fed307166974300815a513a24e
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-03-29 11:32:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -1,32 +1,31 @@
 class Solution {
     public int countPartitions(int[] arr, int diff) {
-        
+
         int totalSum = 0;
-        for(int num : arr)
+
+        for (int num : arr) {
             totalSum += num;
+        }
 
-        if((diff + totalSum) % 2 != 0)
+        // Edge condition
+        if (totalSum - diff < 0 || (totalSum - diff) % 2 != 0) {
             return 0;
+        }
 
-        int target = (diff + totalSum) / 2;
+        int target = (totalSum - diff) / 2;
 
-        int n = arr.length;
+        int[] dp = new int[target + 1];
 
-        int dp[][] = new int[n+1][target+1];
+        dp[0] = 1;
 
-        for(int i=0;i<=n;i++)
-            dp[i][0] = 1;
+        for (int num : arr) {
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=target;j++){
+            for (int j = target; j >= num; j--) {
 
-                if(arr[i-1] <= j)
-                    dp[i][j] = dp[i-1][j-arr[i-1]] + dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
+                dp[j] += dp[j - num];
             }
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 }
