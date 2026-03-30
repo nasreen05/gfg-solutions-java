b926e2afb71cf323be1a89803903886d29af13ea
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-03-30 14:22:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.java
@@ -0,0 +1,50 @@
+
+class Solution {
+
+    public int minCost(int[][] houses) {
+
+        int n = houses.length;
+
+        boolean[] visited = new boolean[n];
+        int[] minDist = new int[n];
+
+        // initialize min distances as max
+        for(int i = 0; i < n; i++) {
+            minDist[i] = Integer.MAX_VALUE;
+        }
+
+        minDist[0] = 0; // start from first house
+        int totalCost = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int u = -1;
+
+            // find minimum distance node
+            for(int j = 0; j < n; j++) {
+                if(!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+
+            visited[u] = true;
+            totalCost += minDist[u];
+
+            // update distances
+            for(int v = 0; v < n; v++) {
+
+                if(!visited[v]) {
+
+                    int cost = Math.abs(houses[u][0] - houses[v][0]) +
+                               Math.abs(houses[u][1] - houses[v][1]);
+
+                    if(cost < minDist[v]) {
+                        minDist[v] = cost;
+                    }
+                }
+            }
+        }
+
+        return totalCost;
+    }
+}
