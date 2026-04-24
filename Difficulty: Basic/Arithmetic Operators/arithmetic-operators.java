49deb2afbc7673ed5bdc4877bfa75e29cd70ab44
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-04-24 20:10:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public static void operations(int x, int y) {
+        // Perform addition
+        int p = x + y;
+
+        // Perform subtraction
+        int q = x - y;
+
+        // Perform multiplication
+        int r = x * y;
+
+        // Perform division
+        int s = x / y;
+
+        // Perform modulo operation
+        int t = x % y;
+
+        // The below code prints the output. Don't change it!
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
