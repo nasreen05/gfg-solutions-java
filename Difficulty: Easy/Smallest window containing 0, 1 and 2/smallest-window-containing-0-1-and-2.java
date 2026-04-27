8b94f087a6ee0757c5e470dd4716b6b87c57ce9a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-04-27 14:10:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.java
+++ Difficulty: Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.java
@@ -1,34 +1,29 @@
 class Solution {
-    public int smallestSubstring(String S) {
+    public int smallestSubstring(String s) {
+        int n = s.length();
+        int[] count = new int[3]; // for '0', '1', '2'
         
-        int n = S.length();
-        int[] count = new int[3]; // for '0','1','2'
-        
-        int left = 0, minLen = Integer.MAX_VALUE;
-        int unique = 0; // count of distinct chars in window
+        int left = 0, distinct = 0;
+        int minLen = Integer.MAX_VALUE;
         
         for (int right = 0; right < n; right++) {
+            int r = s.charAt(right) - '0';
             
-            int idx = S.charAt(right) - '0';
+            if (count[r] == 0) {
+                distinct++;
+            }
+            count[r]++;
             
-            // If this char appears first time
-            if (count[idx] == 0) unique++;
-            
-            count[idx]++;
-            
-            // When we have all 3 characters
-            while (unique == 3) {
-                
+            // Try shrinking window
+            while (distinct == 3) {
                 minLen = Math.min(minLen, right - left + 1);
                 
-                int leftIdx = S.charAt(left) - '0';
-                count[leftIdx]--;
+                int l = s.charAt(left) - '0';
+                count[l]--;
                 
-                // If one character count becomes 0
-                if (count[leftIdx] == 0) {
-                    unique--;
+                if (count[l] == 0) {
+                    distinct--;
                 }
-                
                 left++;
             }
         }
