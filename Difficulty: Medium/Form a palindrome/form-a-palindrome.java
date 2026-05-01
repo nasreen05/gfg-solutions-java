de7580b1cf3a887407bedfa821b2849e7e31de98
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-05-01 13:33:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.java
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -1,25 +1,22 @@
 class Solution {
     int findMinInsertions(String s) {
         int n = s.length();
-        
-        // Reverse the string
         String rev = new StringBuilder(s).reverse().toString();
         
-        // DP array for LCS
-        int[][] dp = new int[n + 1][n + 1];
+        int[] prev = new int[n + 1];
+        int[] curr = new int[n + 1];
         
-        // Compute LCS
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
                 if(s.charAt(i - 1) == rev.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
                 }
             }
+            prev = curr.clone();
         }
         
-        // Minimum insertions needed
-        return n - dp[n][n];
+        return n - prev[n];
     }
 }
