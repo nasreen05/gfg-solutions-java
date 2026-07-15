3fc3b63f3bd545a78ebce638f2c842b83d482584
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-16 02:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Numbers with Given Digit Sum/numbers-with-given-digit-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countWays(int n, int sum) {
+        int[][] dp = new int[n + 1][sum + 1];
+
+        // First digit cannot be 0
+        for (int d = 1; d <= 9; d++) {
+            if (d <= sum) {
+                dp[1][d] = 1;
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int s = 0; s <= sum; s++) {
+                for (int d = 0; d <= 9; d++) {
+                    if (s >= d) {
+                        dp[i][s] += dp[i - 1][s - d];
+                    }
+                }
+            }
+        }
+
+        return dp[n][sum] == 0 ? -1 : dp[n][sum];
+    }
+}
