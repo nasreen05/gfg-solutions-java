4f4b1b40992d778f23772d9c5adca8327c8c4775
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-09-01 03:19:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Smaller in Array/count-smaller-in-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num <= x) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
