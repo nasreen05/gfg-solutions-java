023c8c46f03349d4aaff3c6ec1ce5cc1c57922f5
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-09-03 00:21:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Boolean Value/input-boolean-value.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean a;
+
+        a = sc.nextBoolean();
+
+        // Printing numeric representation
+        System.out.print(a ? 1 : 0);
+    }
+}
